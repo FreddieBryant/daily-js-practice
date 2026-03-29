9bce79a60c08ef311137e6783d99c3374ebd914f
Freddie Bryant <freddie.bryantk4@gmail.com>
2026-03-29 15:54:51 -0400

    Practice arrays with objects in them

--- js-practice/Js-Practice-Objects/index.js
+++ js-practice/Js-Practice-Objects/index.js
@@ -15,3 +15,24 @@ let member = {
 console.log(member.name);
 console.log(member.age);
 console.log(member.status);
+//yes, it feels more organized
+//infortunately this is all i can do today
+//I need save data
+
+let gymMembers = [
+  { name: "Freddie", status: "Gold" },
+  { name: "Sally", status: "Silver" },
+  { name: "Jay", status: "Bronze" },
+];
+
+//loop through gymMembers
+//back tick sentence
+// IN EACH ROUND:
+// GET the current member’s name using gymMembers[i].name.
+// GET the current member’s status using gymMembers[i].status.
+// LOG a sentence: [Name] is a [Status] member.
+// END loop.
+
+for (let i = 0; i < gymMembers.length; i++) {
+  console.log(`${gymMembers[i].name} is a ${gymMembers[i].status} member`);
+}
